bccc722ec385f2c9497220c3ea983c76992a3a9a
valooj <jean.valeriane@gmail.com>
2013-01-25 08:23:46 +0100

    Add firt data members to Protocol Class

--- src/common/Protocol.java
+++ src/common/Protocol.java
@@ -1,5 +1,10 @@
 package common;
 
-public class Protocol {
+public abstract class Protocol {
+	
+	static final public int PORT_PROTOCOL = 12345;
+	
+	static final public int QUERY_TEST = 0x0001;
+	
 
 }
