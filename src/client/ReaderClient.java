8a7b7bdae885d374e797b3d4a2cae0310a8b86ac
nicolabricot <github@nicolabricot.com>
2013-01-25 10:47:48 +0100

    Remove InputSream constructor

--- src/client/ReaderClient.java
+++ src/client/ReaderClient.java
@@ -1,16 +1,12 @@
 package client;
 
 import java.io.DataInputStream;
-import java.io.InputStream;
 
 import common.Reader;
 
 public class ReaderClient extends Reader {
 	
 	public ReaderClient (DataInputStream dis) {
-		super.dis = dis;
-	}
-	public ReaderClient (InputStream is) {
 		super.is = is;
 	}
 
