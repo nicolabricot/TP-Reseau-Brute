90c2cbaa0b38718ef9cbb7cca65677479f671a31
nicolabricot <github@nicolabricot.com>
2013-01-25 09:08:25 +0100

    Change name of PORT

--- src/common/Protocol.java
+++ src/common/Protocol.java
@@ -2,7 +2,7 @@ package common;
 
 public abstract class Protocol {
 	
-	static final public int PORT_PROTOCOL = 12345;
+	static final public int PORT = 12345;
 	
 	static final public int QUERY_TEST = 0x0001;
 	
