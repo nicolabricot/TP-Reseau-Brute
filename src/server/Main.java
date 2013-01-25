fd5a50aff547adef0b0c059bea9ff231dd46e832
nicolabricot <github@nicolabricot.com>
2013-01-25 11:51:29 +0100

    Change display and add respon to get_login

--- src/server/Main.java
+++ src/server/Main.java
@@ -17,14 +17,22 @@ public class Main {
 			while (true) {
 				Socket client = server.accept();
 				Reader reader = new ReaderServer(client.getInputStream());
+				SessionServer session = new SessionServer(client);
+				
 				byte discriminant = reader.readDiscriminant();
 				System.out.println("Server received: " + discriminant);
+				
 				if (discriminant == Protocol.QUERY_TEST) {
 					System.out.println("[QUERY_TEST]");
 				}
 				else if (discriminant == Protocol.GET_LOGIN) {
 					System.out.print("[GET_LOGIN] ");
-					System.out.println(reader.readString());
+					String login = reader.readString();
+					System.out.println(login);
+					if (login.equals("Valoo") || login.equals("Nico"))
+						session.ok();
+					else
+						session.ko();
 				}
 			}
 		
