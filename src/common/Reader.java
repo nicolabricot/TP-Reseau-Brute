3077a096ba7ca0f471bd2c74f2aba8f872488f2c
nicolabricot <github@nicolabricot.com>
2013-01-25 10:46:31 +0100

    Change methods

--- src/common/Reader.java
+++ src/common/Reader.java
@@ -2,17 +2,16 @@ package common;
 
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public abstract class Reader {
 	
-	protected InputStream is;
-	protected DataInputStream dis;
+	protected DataInputStream is;
 	
 	public byte readDiscriminant() {
 		try {
-			return dis.readByte();
-		} catch (IOException e) {
+			return is.readByte();
+		}
+		catch (IOException e) {
 			return (Byte) null;
 		}
 	}
