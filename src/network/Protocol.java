0e884f976534c39de03706d5517f1e129bfb586e
nicolabricot <github@nicolabricot.com>
2013-01-27 20:51:42 +0100

    Rename package common to network

---
+++ src/network/Protocol.java
@@ -0,0 +1,28 @@
+package network;
+
+public abstract class Protocol {
+
+	static final public int PORT = 12345;
+
+	/* COMMON */
+	static final public byte REP_KO = 0x0; // 0
+	static final public byte REP_OK = 0x1; // 1
+	// Test
+	static final public byte QUERY_TEST = 0x2; // 2
+
+	/* CLIENT -> SERVER */
+	static final public byte LOGIN = 0xA; // 10
+	// me
+	static final public byte GET_BRUTE_INFO = 0x14; // 20
+	static final public byte GET_BRUTE_BONUS = 0x15; // 21
+	static final public byte GET_BRUTE_IMG = 0x16; // 22
+	// adversaire
+	static final public byte GET_ADVERSAIRE_INFO = 0x1E; // 30
+	static final public byte GET_ADVERSAIRE_BONUS = 0x1F; // 31
+	static final public byte GET_ADVERSAIRE_IMG = 0x21; // 32
+	// combat
+	static final public byte VICTORY = 0x28; // 40
+	static final public byte DEFEAT = 0x29; // 41
+	static final public byte COMBAT = 0x2A; // 42
+
+}
