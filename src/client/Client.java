cf7742033a0625e153a9faa84547d07e02dcd31b
nicolabricot <github@nicolabricot.com>
2013-01-28 18:56:37 +0100

    Add a request to get bonus[] of a brute
    Additionnaly, insert space between each request

--- src/client/Client.java
+++ src/client/Client.java
@@ -17,37 +17,43 @@ public class Client {
 			Socket client;
 			SessionClient session;
 			
-			System.out.println("Client started\n");
+			System.out.println("Client started");
 			
 			//*
 			client = new Socket(server, Protocol.PORT);
 			session = new SessionClient(client);
-			System.out.println("[" + state++ + "]");
+			System.out.println("\n[" + state++ + "]");
 			session.query_test();
 			client.close();
 			
 			client = new Socket(server, Protocol.PORT);
 			session = new SessionClient(client);
-			System.out.println("[" + state++ + "]");
+			System.out.println("\n[" + state++ + "]");
 			session.login("Valoo");
 			client.close();
 			
 			client = new Socket(server, Protocol.PORT);
 			session = new SessionClient(client);
-			System.out.println("[" + state++ + "]");
+			System.out.println("\n[" + state++ + "]");
 			session.login("unknown");
 			client.close();
 			
 			client = new Socket(server, Protocol.PORT);
 			session = new SessionClient(client);
-			System.out.println("[" + state++ + "]");
+			System.out.println("\n[" + state++ + "]");
 			session.login("Nico");
 			client.close();
 			
 			client = new Socket(server, Protocol.PORT);
 			session = new SessionClient(client);
-			System.out.println("[" + state++ + "]");
+			System.out.println("\n[" + state++ + "]");
 			System.out.println(session.getBruteInfo(3));
+			client.close();
+			
+			client = new Socket(server, Protocol.PORT);
+			session = new SessionClient(client);
+			System.out.println("\n[" + state++ + "]");
+			System.out.println(session.getBruteBonus(0));
 			client.close();
 			
 			//*/
