9934fc1a097648ec305c65ba25adfd553162387c
nicolabricot <github@nicolabricot.com>
2013-01-28 18:56:55 +0100

    Initialize R2D2 brute with bonus

--- src/server/Data.java
+++ src/server/Data.java
@@ -18,16 +18,18 @@ public abstract class Data {
 				new Brute("Stupid bot Tree")
 			);
 	public static final List<Bonus> availableBonus = Arrays.asList(
-				//new Bonus("Chuck Norris", 50, 100, 100, 100),
 				new Bonus("Usain Bolt", 0, 0, 0, 10),
 				new Bonus("Sébastien Chabal", 0, 0, 10, 0 ),
 				new Bonus("Harry Potter", 10, 0, 0, 0),
 				new Bonus("Hermione Granger", 5, 5, 0, 0),
-				new Bonus("Hagrid", 8, 0, 8, -4)
+				new Bonus("Hagrid", 8, 0, 8, -4),
+				new Bonus("Chuck Norris", 50, 100, 100, 100)
 			);
 	
 	public static void populate() {
 		for (int i=0; i<availableBrutes.size(); i++)
 			brutes.add(availableBrutes.get(i));
+		Data.brutes.get(0).addBonus(Data.availableBonus.get(0));
+		Data.brutes.get(0).addBonus(Data.availableBonus.get(3));
 	}
 }
